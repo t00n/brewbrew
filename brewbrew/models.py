5b55a6ebf3509e138cc6b5f5a94a0b8b662cd5d9
Antoine Carpentier <acarpentier@protonmail.com>
2020-12-18 22:18:07 +0100

    [fix] related_name

--- brewbrew/models.py
+++ brewbrew/models.py
@@ -1,52 +1,52 @@
 from django.db import models
 
 class RawMaterial(models.Model):
-	variete = models.CharField(max_length=50)
+    variete = models.CharField(max_length=50)
 
 
 class Producer(models.Model):
-	name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
 
 
 class RawMaterialBatch(models.Model):
-	raw_material = models.ForeignKey(RawMaterial)
-	producer = models.ForeignKey(Producer)
-	batch = models.CharField(max_length=50)
-	facture = models.CharField(max_length=50)
+    raw_material = models.ForeignKey(RawMaterial, on_delete=models.CASCADE)
+    producer = models.ForeignKey(Producer, on_delete=models.CASCADE)
+    batch = models.CharField(max_length=50)
+    facture = models.CharField(max_length=50)
 
 
 class Tank(models.Model):
-	name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
 
 
 class Recipe(models.Model):
-	name = models.CharField(max_length=50)
-	recipe = models.Text()
-	fermentation_time = models.IntegerField()
-	fermentation_tank = models.ForeignKey(Tank)
-	dry_hopping_time = models.IntegerField()
-	dry_hopping_hop1 = models.ForeignKey(RawMaterial)
-	dry_hopping_hop2 = models.ForeignKey(RawMaterial)
-	dry_hopping_hop3 = models.ForeignKey(RawMaterial)
+    name = models.CharField(max_length=50)
+    recipe = models.TextField()
+    fermentation_time = models.IntegerField()
+    fermentation_tank = models.ForeignKey(Tank, on_delete=models.CASCADE)
+    dry_hopping_time = models.IntegerField()
+    dry_hopping_hop1 = models.ForeignKey(RawMaterial, on_delete=models.CASCADE, related_name='recipe_dry_hopping_hop1')
+    dry_hopping_hop2 = models.ForeignKey(RawMaterial, on_delete=models.CASCADE, related_name='recipe_dry_hopping_hop2')
+    dry_hopping_hop3 = models.ForeignKey(RawMaterial, on_delete=models.CASCADE, related_name='recipe_dry_hopping_hop3')
 
 
 class RecipeRawMaterial(models.Model):
-	raw_material = models.ForeignKey(RawMaterial)
-	quantity = models.FloatField()
+    raw_material = models.ForeignKey(RawMaterial, on_delete=models.CASCADE)
+    quantity = models.FloatField()
 
 
 class Brew(models.Model):
-	original_recipe = models.ForeignKey(Recipe)
-	name = models.CharField(max_length=50)
-	recipe = models.Text()
-	fermentation_time = models.IntegerField()
-	fermentation_tank = models.ForeignKey(Tank)
-	dry_hopping_time = models.IntegerField()
-	dry_hopping_hop1 = models.ForeignKey(RawMaterial)
-	dry_hopping_hop2 = models.ForeignKey(RawMaterial)
-	dry_hopping_hop3 = models.ForeignKey(RawMaterial)
+    original_recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    recipe = models.TextField()
+    fermentation_time = models.IntegerField()
+    fermentation_tank = models.ForeignKey(Tank, on_delete=models.CASCADE)
+    dry_hopping_time = models.IntegerField()
+    dry_hopping_hop1 = models.ForeignKey(RawMaterial, on_delete=models.CASCADE, related_name='brew_dry_hopping_hop1')
+    dry_hopping_hop2 = models.ForeignKey(RawMaterial, on_delete=models.CASCADE, related_name='brew_dry_hopping_hop2')
+    dry_hopping_hop3 = models.ForeignKey(RawMaterial, on_delete=models.CASCADE, related_name='brew_dry_hopping_hop3')
 
 
 class BrewRawMaterial(models.Model):
-	raw_material = models.ForeignKey(RawMaterial)
-	quantity = models.FloatField()
+    raw_material = models.ForeignKey(RawMaterial, on_delete=models.CASCADE)
+    quantity = models.FloatField()
