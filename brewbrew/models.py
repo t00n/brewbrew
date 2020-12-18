7073d486ba2c5bb85934437d3e517692308f7885
Antoine Carpentier <acarpentier@protonmail.com>
2020-12-18 21:25:58 +0100

    [add] initial commit. models

---
+++ brewbrew/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+
+class RawMaterial(models.Model):
+	variete = models.CharField(max_length=50)
+
+
+class Producer(models.Model):
+	name = models.CharField(max_length=50)
+
+
+class RawMaterialBatch(models.Model):
+	raw_material = models.ForeignKey(RawMaterial)
+	producer = models.ForeignKey(Producer)
+	batch = models.CharField(max_length=50)
+	facture = models.CharField(max_length=50)
+
+
+class Tank(models.Model):
+	name = models.CharField(max_length=50)
+
+
+class Recipe(models.Model):
+	recipe = models.Text()
+	fermentation_time = models.IntegerField()
+	fermentation_tank = models.ForeignKey(Tank)
+	dry_hopping_time = models.IntegerField()
+	dry_hopping_hop1 = models.ForeignKey(RawMaterial)
+	dry_hopping_hop2 = models.ForeignKey(RawMaterial)
+	dry_hopping_hop3 = models.ForeignKey(RawMaterial)
+
+
+class RecipeRawMaterial(models.Model):
+	raw_material = models.ForeignKey(RawMaterial)
+	quantity = models.FloatField()
+
+
+class Brew(models.Model):
+	original_recipe = models.ForeignKey(Recipe)
+	fermentation_time = models.IntegerField()
+	fermentation_tank = models.ForeignKey(Tank)
+	dry_hopping_time = models.IntegerField()
+	dry_hopping_hop1 = models.ForeignKey(RawMaterial)
+	dry_hopping_hop2 = models.ForeignKey(RawMaterial)
+	dry_hopping_hop3 = models.ForeignKey(RawMaterial)
+
+
+class BrewRawMaterial(models.Model):
+	raw_material = models.ForeignKey(RawMaterial)
+	quantity = models.FloatField()
