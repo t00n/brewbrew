e4124c9e26a402689c2cff6d47ffc816301889be
Antoine Carpentier <acarpentier@protonmail.com>
2020-12-20 17:17:03 +0100

    [add] brew admin

--- brewbrew/admin.py
+++ brewbrew/admin.py
@@ -13,6 +13,15 @@ from .models import (
     RecipeFermentationStep,
     RecipeAdjunct,
     Recipe,
+    BrewMashingIngredientBatch,
+    BrewBrewingStep,
+    BrewBoilingIngredientBatch,
+    BrewWhirlpoolIngredientBatch,
+    BrewYeastBatch,
+    BrewFermentationStep,
+    BrewFermentationAnalysis,
+    BrewAdjunctBatch,
+    Brew,
 )
 
 
@@ -85,4 +94,65 @@ class RecipeAdmin(FieldsetsInlineMixin, admin.ModelAdmin):
         RecipeYeastInline,
         RecipeFermentationStepInline,
         RecipeAdjunctInline,
+    ]
+
+class BrewMashingIngredientBatchInline(admin.TabularInline):
+    model = BrewMashingIngredientBatch
+    extra = 1
+
+
+class BrewBrewingStepInline(admin.TabularInline):
+    model = BrewBrewingStep
+    extra = 1
+
+
+class BrewBoilingIngredientBatchInline(admin.TabularInline):
+    model = BrewBoilingIngredientBatch
+    extra = 1
+
+
+class BrewWhirlpoolIngredientBatchInline(admin.TabularInline):
+    model = BrewWhirlpoolIngredientBatch
+    extra = 0
+
+
+class BrewYeastBatchInline(admin.TabularInline):
+    model = BrewYeastBatch
+    extra = 1
+
+
+class BrewFermentationStepInline(admin.TabularInline):
+    model = BrewFermentationStep
+    extra = 1
+
+
+class BrewFermentationAnalysisInline(admin.TabularInline):
+    model = BrewFermentationAnalysis
+    extra = 2
+
+
+class BrewAdjunctBatchInline(admin.TabularInline):
+    model = BrewAdjunctBatch
+    extra = 0
+
+
+@admin.register(Brew)
+class BrewAdmin(FieldsetsInlineMixin, admin.ModelAdmin):
+    fieldsets_with_inlines = [
+        ('General', {'fields': ['original_recipe', 'batch_name', 'start_date', 'total_quantity']}),
+        ('Mashing', {'fields': ['mashing_water_quantity']}),
+        BrewMashingIngredientBatchInline,
+        ('Brewing', {'fields': ['brew_start_time', 'brew_comments']}),
+        BrewBrewingStepInline,
+        ('Filtration', {'fields': ['filtration_start_time', 'filtration_water_quantity']}),
+        ('Boiling', {'fields': ['boiling_start_time', 'boiling_duration']}),
+        BrewBoilingIngredientBatchInline,
+        ('Whirlpool', {'fields': ['whirlpool_start_time']}),
+        BrewWhirlpoolIngredientBatchInline,
+        ('Cooling', {'fields': ['cooling_start_time', 'cooling_end_time']}),
+        BrewYeastBatchInline,
+        ('Fermentation', {'fields': ['fermentation_tank', 'fermentation_comments']}),
+        BrewFermentationStepInline,
+        BrewFermentationAnalysisInline,
+        BrewAdjunctBatchInline,
     ]
