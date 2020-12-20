795503ee2b668b6ac9deda4a96c04eb394ed0e66
Antoine Carpentier <acarpentier@protonmail.com>
2020-12-20 16:25:49 +0100

    [enh] quantity is always positive

--- brewbrew/models.py
+++ brewbrew/models.py
@@ -194,7 +194,7 @@ class Brew(models.Model):
 
     # general info
     start_date = models.DateField(help_text="Date to start brewing")
-    total_quantity = models.IntegerField(help_text="Quantity of beer obtained with this brew", null=True, blank=True)
+    total_quantity = models.PositiveIntegerField(help_text="Quantity of beer obtained with this brew", null=True, blank=True)
 
     # Mashing
     mashing_ingredients = models.ManyToManyField(
